c617ef90fa17a76d02904207086548439ef2da26
Jose Perdomo <jdperdomos@.unal.edu.co>
2019-11-18 00:55:53 -0500

    LDAP

--- api.js
+++ api.js
@@ -2,26 +2,38 @@ const express = require('express');
 const apiRoutes = express.Router();
 const User = require('./app/models/user');
 const jwt = require('jsonwebtoken');
+var ldap = require('ldapjs');
 
-apiRoutes.get('/signin/:name/:password', (req,res)=>{
-    User.findOne({
-        name: req.params.name
-    }, (err,user)=>{
-        if(err) throw err;
-        if(!user){
-            res.status(401).send('Authentication failed, No User');
-        }else if(user){
-            if(user.password != req.params.password){
-                res.status(401).send('Authentication failed, invalid password');
-            }else{
-                const token = jwt.sign({user}, req.app.get('superSecret'));
-                res.send(token);
+apiRoutes.get('/signin/:name/:password', async (req,res)=>{
+    var client = ldap.createClient({url: 'ldap://172.17.0.1:389'});
+    var dn =`cn=${req.params.name},dc=pugchat,dc=com,dc=co`;
+    var password = req.params.password;    
+    client.bind(dn, password, function (err) {
+      client.unbind();
+      if(err === null){
+        User.findOne({
+            name: req.params.name
+        }, (err,user)=>{
+            if(err) throw err;
+            if(!user){
+                res.status(401).send('Authentication failed, No User');
+            }else if(user){
+                if(user.password != req.params.password){
+                    res.status(401).send('Authentication failed, invalid password');
+                }else{
+                    const token = jwt.sign({user}, req.app.get('superSecret'));
+                    res.send(token);
+                }
+            }{
+    
             }
-        }{
-
-        }
-
-    });
+    
+        });
+      }else{
+        res.status(401).send('Authentication failed, LDAP');
+      };
+    });  
+       
 });
 
 apiRoutes.use((req,res,next)=>{
