bbbdba8e36bf28d9b2199c2d913ec9df87cb1ddb
Jose Perdomo <jdperdomos@.unal.edu.co>
2019-11-18 09:48:05 -0500

    LDAP add user function

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const config = require('./config');
 const User = require('./app/models/user');
 const apiRoutes = require('./api');
+var ldap = require('ldapjs');
 
 app.set('port',process.env.PORT || 3000);
 
@@ -27,12 +28,35 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signup/:name/:password', (req,res)=>{
+
     const testUser = new User({
         name: req.params.name,
         password: req.params.password,
         admin: true
     });
     testUser.save((err)=>{
+        var client = ldap.createClient({url: 'ldap://172.17.0.1:389'});
+        DN = `cn=${req.params.name},dc=pugchat,dc=com,dc=co`;
+        var newUser = {
+            cn: req.params.name,
+            sn: 'SN',
+            uid: 'user',
+            mail: 'user@unal.edu.co',
+            objectClass: 'inetOrgPerson',
+            userPassword: req.params.password
+        }
+        client.bind('cn=admin,dc=pugchat,dc=com,dc=co', '123', function (err) {
+            if(err === null){
+                console.log('adding user');
+                client.add(DN,newUser, function(err) {
+                    if(!err === null)console.log(err);                    
+                });
+                console.log('user added');
+            }else{
+                console.log(err);
+            }
+            client.unbind();
+        });  
         if (err) throw err;
         console.log('User saved succesfully');
         res.send('User saved succesfully');
